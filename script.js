07e3a83389bfd6e3735e39805363152c10233936
EnzoSousa085 <enzo.sousa10@aluno.ifce.edu.br>
2026-04-04 12:44:00 -0300

    update script.js

--- script.js
+++ script.js
@@ -1,52 +1,134 @@
-body{
-margin:0;
-font-family:Arial;
-background:#0f172a;
-color:white;
-text-align:center;
+const words = [
+"médico","professor","cachorro","bola",
+"andar de bicicleta","tirar selfie",
+"crise existencial","dirigir","dançar"
+]
+
+let teams=[]
+let score=[]
+let currentTeam=0
+let currentWord=""
+let attempts=0
+let timer
+let timeLeft
+
+document.getElementById("teamsCount").addEventListener("change",createInputs)
+
+createInputs()
+
+function createInputs(){
+
+let count=document.getElementById("teamsCount").value
+let html=""
+
+for(let i=0;i<count;i++){
+html+=`<input placeholder="Dupla ${i+1}" id="team${i}"><br>`
 }
 
-.container{
-max-width:500px;
-margin:auto;
-padding:20px;
+document.getElementById("teamNames").innerHTML=html
 }
 
-.card{
-background:#111827;
-padding:20px;
-border-radius:12px;
+function startGame(){
+
+let count=document.getElementById("teamsCount").value
+
+teams=[]
+score=[]
+
+for(let i=0;i<count;i++){
+let name=document.getElementById("team"+i).value || "Dupla "+(i+1)
+teams.push(name)
+score.push(0)
 }
 
-input{
-padding:8px;
-margin:5px;
-border-radius:8px;
-border:none;
+document.getElementById("setup").classList.add("hidden")
+document.getElementById("game").classList.remove("hidden")
+
+nextRound()
 }
 
-button{
-padding:12px;
-margin:10px;
-border:none;
-border-radius:10px;
-cursor:pointer;
+function nextRound(){
+
+attempts=0
+currentWord=words[Math.floor(Math.random()*words.length)]
+
+showTurn()
 }
 
-#word{
-font-size:40px;
-margin:40px 0;
-background:#111827;
-padding:30px;
-border-radius:12px;
+function showTurn(){
+
+document.getElementById("team").innerText=teams[currentTeam]
+document.getElementById("word").innerText=currentWord
+
+startTimer()
 }
 
-.top{
-display:flex;
-justify-content:space-between;
-font-size:20px;
+function startTimer(){
+
+clearInterval(timer)
+
+timeLeft=document.getElementById("time").value
+document.getElementById("timer").innerText=timeLeft
+
+timer=setInterval(()=>{
+
+timeLeft--
+document.getElementById("timer").innerText=timeLeft
+
+if(timeLeft<=0){
+wrong()
 }
 
-.hidden{
-display:none;
+},1000)
+
+}
+
+function wrong(){
+
+clearInterval(timer)
+
+attempts++
+
+if(attempts>=teams.length){
+nextTeamStart()
+return
+}
+
+currentTeam++
+
+if(currentTeam>=teams.length)
+currentTeam=0
+
+showTurn()
+}
+
+function correct(){
+
+clearInterval(timer)
+
+score[currentTeam]++
+
+nextTeamStart()
+}
+
+function nextTeamStart(){
+
+currentTeam++
+
+if(currentTeam>=teams.length)
+currentTeam=0
+
+updateScore()
+nextRound()
+}
+
+function updateScore(){
+
+let html="<h3>Placar</h3>"
+
+for(let i=0;i<teams.length;i++){
+html+=teams[i]+" : "+score[i]+"<br>"
+}
+
+document.getElementById("score").innerHTML=html
 }
